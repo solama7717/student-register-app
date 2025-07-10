f4beb7ee601ed4ef5be3e20af5544c6ad13cadeb
solama7717 <solama709@gmail.com>
2025-07-10 13:36:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
 # إعداد الاتصال بقاعدة البيانات
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_HOST'] = 'sql7.freesqldatabase.com'
+app.config['MYSQL_USER'] = 'sql7789263'
+app.config['MYSQL_PASSWORD'] = 'ingy01208320446'
+app.config['MYSQL_DB'] = 'sql7789263'
 
 mysql = MySQL(app)
 
