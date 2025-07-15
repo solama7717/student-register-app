381c6bf0e37faadb434d8533d687b2af9c693a82
solama7717 <solama709@gmail.com>
2025-07-15 17:09:12 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -252,7 +252,7 @@ if (data.siblings === "ليا إخوات") {
     const selectedDay = document.getElementById("days").value;
     const isExempted = (grade === "2ثانوي") || (grade === "1ثانوي" && gender === "ولد");
 
-    fetch("https://student-register-app-2d09.onrender.com/count_students", {
+    fetch("https://localhost:5000/count_students", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ grade, gender, days: selectedDay, time: selectedTime })
